42db6ba0e6b44957c1c518125aaec696847e719d
Ababil IT World <ababilitworld@gmail.com>
2024-06-08 13:53:56 +0600

    ababilitworld/2024-06-08-13-53

---
+++ src/Trait/Standard/Standard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ababilitworld\FlexTraitByAbabilitworld\Trait\Standard;
+
+use Ababilitworld\FlexTraitByAbabilitworld\Trait\Instance\Access\Access;
+use Ababilitworld\FlexTraitByAbabilitworld\Trait\Instance\Instance;
+
+trait Standard 
+{
+    use Instance, Access; 
+}
+
+?>
