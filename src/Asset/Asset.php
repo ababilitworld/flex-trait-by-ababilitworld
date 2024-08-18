f54977f08b3220ad0e9627a536a07485d90ab283
Ababil IT World <ababilitworld@gmail.com>
2024-08-18 18:46:11 +0600

    ababilitworld/2024-08-18-18-46

--- src/Asset/Asset.php
+++ src/Asset/Asset.php
@@ -4,17 +4,18 @@ namespace Ababilitworld\FlexTraitByAbabilitworld\Asset;
 
 trait Asset
 {
-    public static function get_url($url_suffix = null)
+    public function get_url($url_suffix = null)
     {
         // Detect where the package is loaded from
         $reflection = new \ReflectionClass(__CLASS__);
         $package_dir = dirname($reflection->getFileName());
-
+        $package_dir_with_file_name = $reflection->getFileName();
         // Convert to a URL
-        $plugin_dir = plugin_dir_url(dirname($package_dir, 2));
-
-        // Point to the correct assets directory
+        //$plugin_dir = plugin_dir_url(dirname($package_dir, 2));
+        $plugin_dir_url = plugin_dir_url($package_dir_with_file_name);
+       
+        // Point to the correct directory url
         //return $plugin_dir . 'vendor/ababilitworld/flex-pagination-by-ababilitworld/src/Package/';
-        return $plugin_dir . $url_suffix;
+        return $plugin_dir_url . $url_suffix;
     }
 }
